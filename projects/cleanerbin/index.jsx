d233a0d777d5c826467548aff6cfc14f07b1fe5a
tluanga@gmail.com <tluanga@gmail.com>
2019-12-26 17:00:16 +0530

    updated

--- projects/cleanerbin/index.jsx
+++ projects/cleanerbin/index.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from '../../shared/App';
+// import App from '../../shared/App';
+import '../../src/style.css';
 import CategoryCard from '../../shared/ui_components/category/CategoryCard';
 
+
+const App = () => {
+  return (
+    <div className="grid-parent">
+      <CategoryCard title="Men's Fashion" subTitle="Save 10% off start from today." />
+      <CategoryCard title="Men's Fashion" subTitle="Save 10% off start from today." />
+      <CategoryCard title="Men's Fashion" subTitle="Save 10% off start from today." />
+      <CategoryCard title="Men's Fashion" subTitle="Save 10% off start from today." />
+      <CategoryCard title="Men's Fashion" subTitle="Save 10% off start from today." />
+      <CategoryCard title="Men's Fashion" subTitle="Save 10% off start from today." />
+      <CategoryCard title="Men's Fashion" subTitle="Save 10% off start from today." />
+      <CategoryCard title="Men's Fashion" subTitle="Save 10% off start from today." />
+      <CategoryCard title="Men's Fashion" subTitle="Save 10% off start from today." />
+      <CategoryCard title="Men's Fashion" subTitle="Save 10% off start from today." />
+      <CategoryCard title="Men's Fashion" subTitle="Save 10% off start from today." />
+    </div>
+  )
+}
+
+
+
 ReactDOM.render(
-  <CategoryCard title="Men's Fashion" subTitle="Save 10% off start from today." />,
+  <App></App>,
   document.getElementById('app')
 );
 
